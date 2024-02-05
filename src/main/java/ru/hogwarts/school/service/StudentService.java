0f2771b489e8367777280f2cf8ad2f6d9570f3b0
Ekaterina <klarina2409@gmail.com>
2024-02-06 01:12:27 +0300

    только FacultyRest проверять

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -97,7 +97,6 @@ public class StudentService {
 
         avatarRepository.save(avatar);
     }
-
     private String getExtension(String fileName) {
         return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
